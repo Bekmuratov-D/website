bf1b3e71d1cbe22a494e6035a7ba7bbf72705a16
Bekmuratov-MBF <bekmuratovdilmurat18@gmail.com>
2022-11-15 05:27:51 +0300

    start-project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+const Header = () => {
+    return(
+        <nav className='nav'> 
+        <div>
+          <p>Profile</p>
+        </div>
+        <div>
+          <p>Messages</p>
+        </div>
+        <div>
+          <p>News</p>
+        </div>
+        <div>
+          <p>Music</p>
+        </div>
+        <div>
+          <p>Settings</p>
+        </div>
+      </nav>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Profile = () => {
+    return(
+        <div className='content'>
+            <div className='bg-color'></div>
+            Main content
+        </div>
+    )
+}
+
+export default Profile;
